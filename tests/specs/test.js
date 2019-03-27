ab9e2060e88e39875917d55551a222b70cab37ea
mikhsaul <mikhail.sauliak@tele2.com>
2019-03-27 16:14:28 +0300

    initial commit

--- tests/specs/test.js
+++ tests/specs/test.js
@@ -1,9 +1,14 @@
 const assert = require('assert');
 
+const getElement = (selector) => {
+     return $(this.selector);
+  };
+
 describe('webdriver.io page', () => {
     it('should have the right title', () => {        
         browser.url('https://webdriver.io');
-        let text = $('.projectTitle').getText();
+        const title = getElement('.projectTitle');
+        let text = title.getText();
         assert.equal(text, 'Webdriver');
     });
 });
